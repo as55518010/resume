8da90c8cfb7e7be44d2a57f58c5eb86486ef53ff
Derrick <as55518010@yahoo.com.tw>
2024-05-01 17:57:38 +0800

    fix

--- src/components/foot.vue
+++ src/components/foot.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="github-footer">
-    <a class="footer-link" href="https://github.com/as55518010/curriculum_vitae"
+    <a class="footer-link" href="https://github.com/as55518010/resume"
       >項目庫託管在Github上，歡迎star~</a
     >
   </footer>
